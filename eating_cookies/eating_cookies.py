0db0b3db581b67a10b1a641ed376289b0d709384
AshenPheonix <brendlorend@gmail.com>
2019-10-09 10:41:38 -0700

    wip: RPS done

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python
 
 import sys
+from itertools import combinations
 
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+  if n<=1:
+    return 1
+  elif (cache != None and cache <= 1):
+    return 1
+  else:
+    if cache==None:
+      cache=n
+    
+    return (len(list( combinations(range(n),cache) ) ) + eating_cookies(n,cache-1))
+  
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
